ae7cae7f680d408c43e308e78b6b26c5891b9032
Sir Graham <sirgraham@endorasoft.com>
2022-12-19 09:14:07 +0100

    Change dir compile_all_windows.bat for  Visual Studio 2022

--- compile_all_windows.bat
+++ compile_all_windows.bat
@@ -3,7 +3,7 @@ set vctype=%1
 if "%vctype%"=="" (set vctype=Enterprise)
 echo -------------------------------------------------------------
 echo 32 Bits version
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\%vctype%\VC\Auxiliary\Build\vcvarsall.bat" amd64_x86
+call "C:\Program Files\Microsoft Visual Studio\2022\%vctype%\VC\Auxiliary\Build\vcvarsall.bat" amd64_x86
 call ..\GEN\Batch\compile_windows Base\NotAppExample\Platforms\Windows\x86 
 call ..\GEN\Batch\compile_windows Base\AppBaseExample\Platforms\Windows\x86 
 call ..\GEN\Batch\compile_windows Console\BinConnPro\Platforms\Windows\x86
@@ -14,7 +14,7 @@ call ..\GEN\Batch\compile_windows Graphics\Canvas2D\Platforms\Windows\x86
 call ..\GEN\Batch\compile_windows Graphics\UI_Options\Platforms\Windows\x86 
 echo -------------------------------------------------------------
 echo 64 Bits version
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\%vctype%\VC\Auxiliary\Build\vcvarsall.bat" amd64 
+call "C:\Program Files\Microsoft Visual Studio\2022\%vctype%\VC\Auxiliary\Build\vcvarsall.bat" amd64 
 call ..\GEN\Batch\compile_windows Base\NotAppExample\Platforms\Windows\x64 
 call ..\GEN\Batch\compile_windows Base\AppBaseExample\Platforms\Windows\x64 
 call ..\GEN\Batch\compile_windows Console\BinConnPro\Platforms\Windows\x64
