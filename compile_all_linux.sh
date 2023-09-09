4a0f20aa9992b3e968fdac067025e935625ccefa
Sir Graham <sirgraham@endorasoft.com>
2023-09-09 13:23:35 +0200

    change TAS to ATS

--- compile_all_linux.sh
+++ compile_all_linux.sh
@@ -17,7 +17,7 @@ printf "Examples Console Linux PC x64 ... \n"
 sh ./../GEN/Batch/compile_linux.sh Console/BinConnPro/Platforms/Linux /x64 binconnpro
 sh ./../GEN/Batch/compile_linux.sh Console/Databases/Platforms/Linux /x64 databases
 sh ./../GEN/Batch/compile_linux.sh Console/MiniWebServer/Platforms/Linux /x64 miniwebserver
-sh ./../GEN/Batch/compile_linux.sh Console/TSA/Platforms/Linux /x64 tsa 
+sh ./../GEN/Batch/compile_linux.sh Console/ATS/Platforms/Linux /x64 ats 
 
 printf "Examples Graphics Linux PC x64 ... \n"
 sh ./../GEN/Batch/compile_linux.sh Graphics/Canvas2D/Platforms/Linux /x64 canvas2d
