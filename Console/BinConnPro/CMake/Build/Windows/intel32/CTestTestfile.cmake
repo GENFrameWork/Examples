ca054789dc28f2cdf8a42fe24ec4fc9d141117a6
Sir Graham <sirgraham@endorasoft.com>
2026-04-12 14:54:52 +0200

    Changes directories Platform <> CMakes -> Build

---
+++ Console/BinConnPro/CMake/Build/Windows/intel32/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: E:/Projects/GEN_FrameWork/Examples/Console/BinConnPro/CMake
+# Build directory: E:/Projects/GEN_FrameWork/Examples/Console/BinConnPro/CMake/Build/Windows/intel32
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
